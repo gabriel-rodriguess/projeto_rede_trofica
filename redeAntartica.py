2dbc33a5870647bc1b5dd557ccce252afcaf5e06
Gabriel Gustavo Pereira Rodrigues dos Santos <38788527+zManshu@users.noreply.github.com>
2019-04-16 19:52:15 -0300

    Add Grafico

--- redeAntartica.py
+++ redeAntartica.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import matplotlib.pyplot as plt
 
 # In[ ]:
 
@@ -17,7 +18,7 @@ peixe = 15.0
 foca = 15.0
 
 #Taxa de infecção da foca por poluição
-poluicao = 1.9
+poluicao = 1.8
 
 v = [] #Definindo vetor que armazena os parâmetros
 #deltaT = 0.01 #Variação de deltaT
@@ -25,27 +26,27 @@ deltaT = 0.01
 K = 100.0 #Capacidade de suporte ao meio
 tempo = 0.0
 
-v.append(0.02)
-v.append(0.01)
-v.append(0.094)
-v.append(0.01)
-v.append(0.01)
-v.append(0.09)
-v.append(0.01)
-v.append(0.01)
-v.append(0.04)
-v.append(0.06)
-v.append(0.01)
-v.append(0.02)
-v.append(0.03)
-v.append(0.009)
+v.append(0.02) #0
+v.append(0.01)#1
+v.append(0.094)#2
+v.append(0.01)#3
+v.append(0.01)#4
+v.append(0.090)#5
+v.append(0.01)#6
+v.append(0.01)#7
+v.append(0.04)#8
+v.append(0.06)#9
+v.append(0.01)#10
+v.append(0.02)#11
+v.append(0.03)#12
+v.append(0.009)#13
 
 #gravar os dois
-arq.write("Tempo Fitoplanton Camarao Peixe Pinguim Foca \n")
+#arq.write("Tempo Fitoplanton Camarao Peixe Pinguim Foca \n")
 arq.write("{}  {}   {}  {}  {}  {}\n".format("%.2f" % tempo, fitoplanton, camarao,peixe , pinguim, foca))
 
 
-while(tempo < 300):
+while(tempo < 100):
     tempo += deltaT
     #Taxa de variação de cada em espécie em função das equações de Lotka-Volterra
     dFitoplanton = fitoplanton * (fotossintese - fotossintese*fitoplanton/K - v[0]*camarao - v[1]*peixe - temp_agua_aumento) * deltaT
@@ -62,3 +63,22 @@ while(tempo < 300):
 
 
     arq.write("{}  {}   {}  {}  {}  {}\n".format("%.2f" % tempo, fitoplanton, camarao,peixe , pinguim, foca))
+
+with open('antartica.txt') as data:
+    lines = data.readlines()
+    a = [float(line.split()[3]) for line in lines]
+    b = [float(line.split()[4]) for line in lines]
+    c = [float(line.split()[2]) for line in lines]
+    d = [float(line.split()[5]) for line in lines]
+    e = [float(line.split()[1]) for line in lines]
+
+plt.ylabel('N. Animais')
+plt.xlabel('Tempo')
+
+plt.plot(a, color='blue', linewidth=2.2, label='Peixe')
+plt.plot(b, color='green', linewidth=2.2, label='Pinguim')
+plt.plot(c, color='yellow', linewidth=2.2, label='Camarao')
+plt.plot(d, color='grey', linewidth=2.2, label='Foca')
+plt.plot(e, color='purple', linewidth=2.2, label='Fitoplanton')
+plt.legend()
+plt.show()
